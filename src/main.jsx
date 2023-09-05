ab1410cd8d5d630446f37dc3e073f2b0dac57951
Gustavo Soares <guusoaresss@icloud.com>
2023-09-04 22:59:40 -0300

    Const Router

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,28 @@ import Produtos from './routes/Produtos/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 import Error from './routes/Error/index.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/> 
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
