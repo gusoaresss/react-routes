0bc3b6c9e9de5d3b69a2fc7b553a85f3f9fdf02a
Gustavo Soares <guusoaresss@icloud.com>
2023-09-04 23:21:48 -0300

    Adicionando estilo + redirecionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Menu from './Components/Menu'
+import Rodape from './Components/Rodape'
 
 function App(){
 
   return(
     <>
+      <Menu/>
       <Outlet/>
+      <Rodape/>
     </>
   )
 }
